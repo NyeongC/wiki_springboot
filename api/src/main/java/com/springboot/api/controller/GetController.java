bbd960470f563cb99b2cdb2a130cc79782311211
NyeongC <cjfsud23@naver.com>
2023-10-20 14:34:32 +0900

    P.65 @RequestParam

--- api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -2,6 +2,8 @@ package com.springboot.api.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/vi/get-api")
 public class GetController {
@@ -17,5 +19,16 @@ public class GetController {
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
         return variable;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
     }
 }
