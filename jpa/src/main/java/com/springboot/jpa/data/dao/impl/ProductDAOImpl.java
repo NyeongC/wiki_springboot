43037ee09b2b3986cb134f987b3524b1638154ed
NyeongC <cjfsud23@naver.com>
2023-10-21 12:13:00 +0900

    P. 117

---
+++ jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,27 @@
+package com.springboot.jpa.data.dao.impl;
+
+import com.springboot.jpa.data.dao.ProductDAO;
+import com.springboot.jpa.data.entity.Product;
+
+public class ProductDAOImpl implements ProductDAO {
+
+    @Override
+    public Product insertProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product selectProduct(Long number) {
+        return null;
+    }
+
+    @Override
+    public Product updateProductName(Long number, String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+
+    }
+}
